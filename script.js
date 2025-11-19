2605f1eceb65678271e10c436a9996fb2a050b65
Premtim <premtim.berisha@hotmail.com>
2025-11-19 16:18:21 +0100

    Changes squares with different colors

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 let container = document.getElementById('container');
 
+let coloredSquare = [];
+
 function createGrid(size){
     container.innerHTML = '';
 
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
+    coloredSquare = [];
+
     for (let i=0;i<size*size;i++){
         const square = document.createElement('div');
 
         square.classList.add('grid-square');
         square.addEventListener('mouseenter', () => {
-            square.style.backgroundColor = 'black';
+            let r = Math.floor(Math.random()*256);
+            let g = Math.floor(Math.random()*256);
+            let b = Math.floor(Math.random()*256);
+            square.style.backgroundColor =  `rgb(${r},${g},${b})`;
+
+            coloredSquare.push(square);
+
+            if(coloredSquare.length > 10){
+                let oldestSquare = coloredSquare.shift();
+                oldestSquare.style.backgroundColor = 'white';
+            }
         });
-        
+
         container.appendChild(square);
     }
 }
@@ -24,7 +38,10 @@ let reset = document.getElementById('reset');
 reset.addEventListener('click',  () => {
     let newSize = +prompt('Choose new size', '');
 
-    if(Number.isInteger(newSize >= 1 && newSize <= 100)){
+    if(!newSize){
+        return ;
+    }
+    if(Number.isInteger (newSize) && newSize >= 1 && newSize <= 100){
         createGrid(newSize);
     } else {
         alert("Enter a whole number from 1 to 100!");
